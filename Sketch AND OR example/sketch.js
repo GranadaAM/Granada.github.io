5e452a80418c137da755ac0e8001ac9eef1d50e8
Alexander Madsen <alexandermadsen04@gmail.com>
2020-10-29 14:48:01 +0100

    jajaja

---
+++ Sketch AND OR example/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(800, 800);
+  }
+  
+  function draw() {
+    background(220);
+    
+    if (mouseX < 400 && mouseY < 400){
+        circle(mouseX, mouseY, 30)
+    }
+    if (mouseX > 400 && mouseY < 400){
+        circle(mouseX, mouseY, 60)
+    }
+    if (mouseX > 400 && mouseY > 400){
+        text('xD', mouseX, mouseY)
+    }
+    if (mouseX < 400 && mouseY > 400){
+        text('haha', mouseX, mouseY)
+    }
+  }
