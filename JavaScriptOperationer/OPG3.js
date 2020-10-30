8b2594a5a9f952787dc6688034124524a651cade
Alexander Madsen <alexandermadsen04@gmail.com>
2020-10-30 10:24:01 +0100

    opg 3 med alle tabeller fra 2-10

--- JavaScriptOperationer/OPG3.js
+++ JavaScriptOperationer/OPG3.js
@@ -1,4 +1,8 @@
-for (i=0; i<=20; i++){
-    if (i%2 == 0)
-    alert(i); //brug document.write i html kode for at display ordentligt
+for(let j=2; j<=10; j++){
+    for (i=0; i<=j*10; i++){
+        if (i%j == 0)
+        alert(i); //brug document.write i html kode for at display ordentligt
+    }
+    //brug igen document.write for at skabe plads mellem tabellerne
 }
+
